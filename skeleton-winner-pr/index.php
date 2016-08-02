dc537e60f092d17b4985ed07994ade97f3f6b2b8
R.Izmaylov <mayer.elen@gmail.com>
2016-08-02 21:43:36 +0700

    skeleton-winner-pr metrika

--- skeleton-winner-pr/index.php
+++ skeleton-winner-pr/index.php
@@ -4,7 +4,7 @@
 
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <title>Часы Skeleton Winner</title>
+    <title>Skeleton Winner - элитные часы</title>
 
     <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1">
 
@@ -22,7 +22,7 @@
     <link rel="stylesheet" type="text/css" href="fonts/roboto.css">
     <link href="css/ComebackerAlert.css" rel="stylesheet" type="text/css"/>
     <!-- Yandex.Metrika counter -->
-    <script type="text/javascript"> (function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter38627605 = new Ya.Metrika({ id:38627605, clickmap:true, trackLinks:true, accurateTrackBounce:true }); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = "https://mc.yandex.ru/metrika/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks"); </script> <noscript><div><img src="https://mc.yandex.ru/watch/38627605" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+    <script type="text/javascript"> (function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter38828905 = new Ya.Metrika({ id:38828905, clickmap:true, trackLinks:true, accurateTrackBounce:true }); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = "https://mc.yandex.ru/metrika/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks"); </script> <noscript><div><img src="https://mc.yandex.ru/watch/38828905" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
     <!-- /Yandex.Metrika counter -->
 
     <script>
